64c5f8bc58082e8f194f6d666f156b8c954160a8
Sumit Khandelwal <synergy2411@outlook.com>
2024-11-06 11:02:48 +0530

    new post added

--- 01-graphql-basics/src/graphql/resolvers/Mutation.js
+++ 01-graphql-basics/src/graphql/resolvers/Mutation.js
@@ -1,4 +1,5 @@
 import { v4 } from "uuid";
+import { GraphQLError } from "graphql";
 
 const Mutation = {
   createUser: (parent, args, { db }, info) => {
@@ -10,6 +11,22 @@ const Mutation = {
     };
     db.users.push(newUser);
     return newUser;
+  },
+  createPost: (parent, args, { db }, info) => {
+    const { title, body } = args.data;
+    const position = db.users.findIndex((user) => user.id === args.author);
+    if (position === -1) {
+      throw new GraphQLError("User does not exist for Id - " + args.author);
+    }
+    const newPost = {
+      id: v4(),
+      title,
+      body,
+      published: false,
+      author: args.author,
+    };
+    db.posts.push(newPost);
+    return newPost;
   },
 };
 
