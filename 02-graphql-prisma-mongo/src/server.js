917ea20d495b6338f03d5b8e6bad82bda8ac8b70
Sumit Khandelwal <synergy2411@outlook.com>
2024-11-06 15:51:17 +0530

    more prisma operations

--- 02-graphql-prisma-mongo/src/server.js
+++ 02-graphql-prisma-mongo/src/server.js
@@ -3,16 +3,31 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const createdUser = await prisma.user.create({
-    data: {
-      name: "Monica Geller",
-      age: 22,
-      email: "monica@test",
-      password: "monica123",
-      role: "ADMIN",
-    },
-  });
-  console.log(createdUser);
+  //   await prisma.user.delete({
+  //     where: {
+  //       email: "sumit@test",
+  //     },
+  //   });
+  //   const allUsers = await prisma.user.findMany();
+  //   console.log(allUsers);
+  //   const allUsers = await prisma.user.findMany({
+  //     take: 1,
+  //     skip: 1,
+  //     orderBy: {
+  //       name: "asc",
+  //     },
+  //   });
+  //   console.log(allUsers);
+  //   const createdUser = await prisma.user.create({
+  //     data: {
+  //       name: "Monica Geller",
+  //       age: 22,
+  //       email: "monica@test",
+  //       password: "monica123",
+  //       role: "ADMIN",
+  //     },
+  //   });
+  //   console.log(createdUser);
 }
 
 main()
